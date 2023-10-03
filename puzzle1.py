79ff5b7882d08931f5a34d5eadc7e8c1aa7b1a6b
victorfdzlucas <144890544+victorfdzlucas@users.noreply.github.com>
2023-10-03 11:40:23 +0200

    Update puzzle1.py

--- puzzle1.py
+++ puzzle1.py
@@ -1,1 +1,13 @@
-print("Hola")
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+try:
+    print("Pasa la tarjeta NFC cerca del lector...")
+    while True:
+        id, text = reader.read()
+        print("Tarjeta detectada con el ID:", id)
+        print("Texto en la tarjeta:", text)
+finally:
+    GPIO.cleanup()
